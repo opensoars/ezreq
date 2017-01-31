ac61761b6338ae598d555c008a2f19709edd1832
s <sam.opensoars@gmail.com>
2017-01-31 19:29:38 +0100

    index example doc

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const DELETE =  require('./DELETE');
 
 /**
  * Module object that gets required.
+ * @example
+ * import {GET, PUT, POST, DELETE} from 'ezreq'
  */
 const ezreq = {
   GET, PUT, POST, DELETE
